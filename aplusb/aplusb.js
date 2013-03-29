44177dfef8d39d2a0c0345fbda8495f538c6c757
Dax Reyes <raikage@gmail.com>
2013-03-29 23:58:44 +0800

    added operation (+, etc)

--- aplusb/aplusb.js
+++ aplusb/aplusb.js
@@ -4,6 +4,7 @@ if (Meteor.isClient) {
            Session.set("number", Math.floor(Math.random()*10) + 1);
            Session.set("range", Math.floor(Math.random()*10) + 1);
            Session.set("repeat", Math.floor(Math.random()*10) + 1);
+           Session.set("operation", "+");
        });
     
     Template.generator.events({
@@ -25,16 +26,21 @@ if (Meteor.isClient) {
         return Session.get("repeat");
     };
 
-    Template.generated_table.items = function (){
+    Template.generator.operation = function(){
+        return Session.get("operation");
+    }
+
+    Template.generated_table.data = function (){
         var number = Session.get("number");
         var range = Session.get("range");
         var repeat = Session.get("repeat");
+        var operation = Session.get("operation");
 
         var items = []
         for (var i=0;i<repeat; i++){
             items.push({left:_.random(1,range), right: number});
         }
-        return items;
+        return {items: items, operation: operation};
     }
 
 }
