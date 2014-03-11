9ecf4df2136b7975c1b3c9afe7c47a65df80f0e8
Dax <dax@Daxs-MacBook-Pro.local>
2014-03-11 13:59:26 +0800

    added closing of sideMenu. Still have rendering issues

--- ionic-sample/ionic-sample.js
+++ ionic-sample/ionic-sample.js
@@ -82,6 +82,29 @@ if (Meteor.isClient) {
       }
   ]);
 
+//   ngMeteor.factory('autorun', function() {
+//   return function(scope, fn) {
+//     // wrapping around Deps.autorun
+//     var comp = Deps.autorun(function(c) {
+//       fn(c);
+      
+//       // this is run immediately for the first call
+//       // but after that, we need to $apply to start Angular digest
+//       if (!c.firstRun) setTimeout(function() {
+//         // wrap $apply in setTimeout to avoid conflict with
+//         // other digest cycles
+//         scope.$apply();
+//       }, 0);
+//     });
+//     // stop autorun when scope is destroyed
+//     scope.$on('$destroy', function() {
+//       comp.stop();
+//     });
+//     // return autorun object so that it can be stopped manually
+//     return comp;
+//   };
+// });
+
   ngMeteor.controller('MainCtrl', ['$scope', '$collection', '$rootScope', '$ionicModal', '$timeout',
     function ($scope, $collection, $rootScope, $ionicModal, $timeout) {
 
